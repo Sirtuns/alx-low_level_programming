6d625a6aab24f9f311896f5e244f7b16e099a6ad
sirtuns <sirtuns4real@gmail.com>
2023-02-16 03:21:33 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,15 @@
 /**
  * main - Entry point
  *
- * Author: Sammykingx
+ * Author: Sirtuns
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle\"";
-	
+
 	puts(string);
 
-	/* Tried printing it directly */
-
-	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
