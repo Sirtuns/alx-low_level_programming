326e384b8c087c4d18664c5103dd5299b1441dac
Oloyede Olatunde <98672074+Sirtuns@users.noreply.github.com>
2023-04-12 04:20:17 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
  * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
- * @filename: The name of the file to read from
+ * @filename: The name of the file to read
  * @letters: The number of characters it should print to the STDOUT
  * Return: Returns the number of characters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fild = 0, chk = 0;
+	ssize_t fileid = 0, checkk = 0;
 	char *buffer;
 
 	if (!filename || !letters)
 		return (0);
 
-	fild = open(filename, O_RDONLY);
-	if (fild < 0)
+	fileid = open(filename, O_RDONLY);
+	if (fileid < 0)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters));
 	if (!buffer)
 		return (0);
 
-	chk = read(fild, buffer, letters);
-	chk = write(STDOUT_FILENO, buffer, chk);
-	if (chk < 0)
+	check = read(fileid, buffer, letters);
+	check = write(STDOUT_FILENO, buffer, check);
+	if (check < 0)
 		return (0);
 
-	close(fild);
+	close(fileid);
 	free(buffer);
-	return (chk);
+	return (check);
 }
